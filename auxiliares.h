5c034192cfe17f90569f01718ab617f04a58b7f1
homeless51 <49304700+homeless51@users.noreply.github.com>
2019-04-08 23:25:41 +0100

    Update auxiliares.h

--- auxiliares.h
+++ auxiliares.h
@@ -1,10 +1,9 @@
-#ifndef PROJ_AUXILIARES_H
-#define PROJ_AUXILIARES_H
+#ifndef PROJETO_AUXILIARES_H
+#define PROJETO_AUXILIARES_H
 
-ESTADO reset(ESTADO e,char linha[]);
-ESTADO jogada(ESTADO e, char linha[]);
 int valida (ESTADO e, int l, int c);
 ESTADO jogar (ESTADO e, int l, int c);
-ESTADO posvalidas (ESTADO e, int l, int c);
-#endif //PROJ_AUXILIARES_H
+ESTADO posvalidas (ESTADO e);
 
+
+#endif //PROJETO_AUXILIARES_H
