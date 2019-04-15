d6d79add0081e0aa90bfb045a8ce25278c16d965
Deejay139 <49171935+Deejay139@users.noreply.github.com>
2019-04-15 12:18:48 +0100

    Update estado.c

--- estado.c
+++ estado.c
@@ -5,7 +5,38 @@
 #include <stdio.h>
 
 // exemplo de uma função para imprimir o estado (Tabuleiro)
+void printa(ESTADO e) {
+    char c = ' ';
+    if (e.peca== VALOR_X) printf("Jogador X\n");
+    else printf("Jogador O \n");
+    if (e.modo == 0) printf("  Modo Manual\n");
+    else printf("Automatico\n\n");
 
+    printf("  0 1 2 3 4 5 6 7\n");
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (j == 0) printf("%d ",i);
+            switch (e.grelha[i][j]) {
+                case VALOR_O: {
+                    c = 'O';
+                    break;
+                }
+                case VALOR_X: {
+                    c = 'X';
+                    break;
+                }
+                case VAZIA: {
+                    c = '-';
+                    break;
+                }
+            }
+            printf("%c ", c);
+
+        }
+        printf("\n");
+    }
+    printf("X = %d  O = %d ", numPecas_X(e), numPecas_O(e));
+}
 ESTADO reset (ESTADO e,char linha[]) {
     int i=1, j;
     while (linha[i]==' ')
@@ -36,47 +67,99 @@ ESTADO reset (ESTADO e,char linha[]) {
 
     return e;
 }
-
-void printa(ESTADO e)
-{
-    char c = ' ';
-    if (e.peca== VALOR_X) printf("Jogador X\n");
-    else printf("Jogador O \n");
-    if (e.modo == 0) printf("Manual\n");
-    else printf("Automatico\n\n");
-
+void posvalidas (ESTADO e){
+    int l,c;
     printf("  0 1 2 3 4 5 6 7\n");
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            if (j == 0) printf("%d ",i);
-            switch (e.grelha[i][j]) {
-                case VALOR_O: {
-                    c = 'O';
-                    break;
-                }
-                case VALOR_X: {
-                    c = 'X';
-                    break;
-                }
-                case VAZIA: {
-                    c = '-';
-                    break;
-                }
-            }
-            printf("%c ", c);
-
+    for(l=0; l<8 ; l++){
+        for(c=0; c<8; c++){
+            if(c==0) printf("%d ",l);
+            if (e.grelha[l][c]==VALOR_X) printf("X ");
+            else if(e.grelha[l][c]==VALOR_O) printf("O ");
+            else if(valida(e,l,c)==1) printf(". ");
+            else printf("- ");
         }
         printf("\n");
     }
-
 }
-
-
-
-
-
-
-
+void sugestao (ESTADO e) {
+    int i,j,r = 0;
+    printf("  0 1 2 3 4 5 6 7\n");
+    for(i=0;i<8;i++){
+        for(j=0;j<8;j++){
+            if(j==0) printf("%d ",i);
+            if (valida(e,i,j) && r == 0){
+                printf("? ");
+                r++;
+            }
+            else if (e.grelha[i][j]==VALOR_X) printf("X ");
+            else if(e.grelha[i][j]==VALOR_O) printf("O ");
+            else printf("- ");
+        }
+        printf("\n");
+    }
+}
+ESTADO jogar (ESTADO e, int l, int c) {
+    int i, j;
+    VALOR h;
+    if (e.peca == VALOR_X) h = VALOR_O;
+    else if(e.peca==VALOR_O) h = VALOR_X;
+    else h=VAZIA;
+    if (valida(e,l,c)==0);
+    else {
+        if (validaNorte(e, l, c)) {
+            i = l - 1;
+            j = c;
+            for (; e.grelha[i][j] == h; i--) {
+                e.grelha[i][j] = e.peca;}
+        }
+        if (validaSul(e, l, c)) {
+            i = l + 1;
+            j = c;
+            for (; e.grelha[i][j] == h; i++) {
+                e.grelha[i][j] = e.peca;}
+        }
+        if (validaOeste(e, l, c)) {
+            i = l;
+            j = c - 1;
+            for (; e.grelha[i][j] == h; j--) {
+                e.grelha[i][j] = e.peca;}
+        }
+        if (validaEste(e, l, c)) {
+            i = l;
+            j = c + 1;
+            for (; e.grelha[i][j] == h; j++) {
+                e.grelha[i][j] = e.peca;}
+        }
+        if (validaNoroeste(e, l, c)) {
+            i = l - 1;
+            j = c - 1;
+            for (;e.grelha[i][j] == h; i--, j--) {
+                e.grelha[i][j] = e.peca;}
+        }
+        if (validaNordeste(e, l, c)) {
+            i = l - 1;
+            j = c + 1;
+            for (;e.grelha[i][j] == h; i--, j++) {
+                e.grelha[i][j] = e.peca;}
+        }
+        if (validaSudoeste(e, l, c)) {
+            i = l + 1;
+            j = c - 1;
+            for (; e.grelha[i][j] == h; i++, j--) {
+                e.grelha[i][j] = e.peca;}
+        }
+        if (validaSudeste(e, l, c)){
+            i = l + 1;
+            j = c + 1;
+            for (;e.grelha[i][j] == h; i++, j++) {
+                e.grelha[i][j] = e.peca;}
+        }
+        e.grelha[l][c] = e.peca;
+        if(e.peca==VALOR_X) e.peca=VALOR_O;
+        else e.peca=VALOR_X;
+    }
+    return e;
+}
 ESTADO ler(ESTADO e, char s[]) {
     char s1;
     FILE *reversi = fopen(s, "r");
@@ -104,7 +187,6 @@ ESTADO ler(ESTADO e, char s[]) {
     return e;
 
 }
-
 void escrever(ESTADO e, char s[]) {
     char s1,s2;
     FILE *reversi = fopen(s, "W");
@@ -128,3 +210,5 @@ void escrever(ESTADO e, char s[]) {
     }
     fclose(reversi);
 }
+
+
