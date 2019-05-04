18182a2c75dcd63060fdb7be2326c7f3dd17110a
homeless51 <49304700+homeless51@users.noreply.github.com>
2019-05-04 21:57:00 +0100

    Create bot.h

---
+++ bot.h
@@ -0,0 +1,14 @@
+
+// Created by carlos on 03-05-2019.
+//
+#include "estado.h"
+
+#ifndef PROJETO_BOT_H
+#define PROJETO_BOT_H
+
+
+ESTADO automatico (ESTADO e,char linha[]);
+ESTADO bot_1 (ESTADO e) ;
+
+
+#endif //PROJETO_BOT_H
