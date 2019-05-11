98bd72972962ecdd8f4523c5ed9a4b0fc3050c72
Deejay139 <49171935+Deejay139@users.noreply.github.com>
2019-05-11 23:25:23 +0100

    Update bot.h

--- bot.h
+++ bot.h
@@ -1,14 +1,27 @@
 
+
 // Created by carlos on 03-05-2019.
 //
 #include "estado.h"
 
 #ifndef PROJETO_BOT_H
 #define PROJETO_BOT_H
+typedef struct par {
+    int l;
+    int c;
+}PAR;
 
 
-ESTADO automatico (ESTADO e,char linha[]);
+typedef struct lista {
+    int sp;
+    PAR valores[64];
+}LISTA;
+
+
+
+
 ESTADO bot_1 (ESTADO e) ;
+void numposicoesval (ESTADO e, LISTA *p);
 
 
 #endif //PROJETO_BOT_H
